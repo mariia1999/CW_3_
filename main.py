9f2bdc05a43cec1510928df172d9fdf23d2ad65e
Maria Lomets <marusya.lomets@gmail.com>
2024-05-02 21:51:39 +0300

    first commit

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from src.utils import get_data, filter_data, sort_operations, format_date, forma
 load_data = get_data()
 filter_ = filter_data(load_data)
 sort_ = sort_operations(filter_)
-#format_ = format_date(sort_)
+format_ = format_date(sort_)
 format_tr = format_transaction(load_data)
 
 
 #print_transactions(sort_)
 
 #program = print_transactions(load_data)
-print(format_tr)
+print(format_)
